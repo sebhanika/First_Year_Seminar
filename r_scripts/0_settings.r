9585a29cf91f16b508b045e1c5d37ae07986aed9
sebhanika <hanikasebastian@gmail.com>
2023-10-12 13:37:11 +0200

    cspell deactivate

--- r_scripts/0_settings.r
+++ r_scripts/0_settings.r
@@ -1,13 +1,12 @@
 # Title: Settings file
 # Date: 2023-09-22
 # Purpose: This is a settings file that will be tracked by git
+# /* cSpell:disable */
 
 library(ggthemes)
-
 options(scipen = 999)
 
 # GGplot Settings  --------------
-
 
 basic_theme <- theme_set(theme_base() +
     theme(
