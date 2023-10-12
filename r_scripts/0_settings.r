79e2fe08b420db8f4fbd8d65de689c04eb04cc0a
sebhanika <hanikasebastian@gmail.com>
2023-10-12 12:08:58 +0200

    include ggplot settings

--- r_scripts/0_settings.r
+++ r_scripts/0_settings.r
@@ -2,7 +2,18 @@
 # Date: 2023-09-22
 # Purpose: This is a settings file that will be tracked by git
 
+library(ggthemes)
 
 options(scipen = 999)
 
 # GGplot Settings  --------------
+
+
+basic_theme <- theme_set(theme_base() +
+    theme(
+        axis.line = element_line(colour = "black"),
+        axis.title = element_text(size = 16, color = "grey30"),
+        axis.text = element_text(size = 16, color = "grey30"),
+        legend.text = element_text(size = 18, color = "grey30"),
+        legend.title = element_blank()
+    ))
