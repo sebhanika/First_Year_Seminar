cf17e077b0f2b3fac10906e932a1f1a1b2360165
sebhanika <hanikasebastian@gmail.com>
2023-10-17 13:21:12 +0200

    del un projections

--- r_scripts/projections.r
+++ r_scripts/projections.r
@@ -13,34 +13,6 @@ library(ggplot2)
 library(ggthemes)
 library(tidyr)
 
-# UN projections --------------
-
-# Source: https://population.un.org/wpp/Download/
-
-df_list <- lapply(excel_sheets("data/UN_PPP2022_Output_PopPerc.xlsx"), function(x) {
-    read_excel("data/UN_PPP2022_Output_PopPerc.xlsx", sheet = x, skip = 16)
-})
-
-df_list[[6]] <- NULL
-
-
-dat <- lapply(df_list, function(x) {
-    x %>%
-        janitor::clean_names() %>%
-        filter(region_subregion_country_or_area == "EUROPE") %>%
-        mutate(x65 = as.numeric(x65)) %>%
-        select(c(variant, year, x65))
-})
-
-
-dat_comb <- do.call(dplyr::bind_rows, dat)
-
-dat_comb %>%
-    ggplot(aes(x = year, y = x65, color = variant)) +
-    geom_line() +
-    theme_base()
-
-
 # Eurostat projections --------------
 
 # get eurostat
